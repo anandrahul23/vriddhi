0b06c063a9c9774eebf0a63a18e454b7bbf647dc
Rahul Anand <anand.rahul2886@gmail.com>
2023-02-05 18:55:51 +0000

    remove db dependency

--- src/main/java/com/anand/vriddhi/VriddhiApplication.java
+++ src/main/java/com/anand/vriddhi/VriddhiApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VriddhiApplication {
 
     public static void main(String[] args) {
-
-        System.out.println("Starting the Vriddhi Application");
         SpringApplication.run(VriddhiApplication.class, args);
     }
 
