ef4d216ccd350b64fb50a7a8d8b389088824b42a
Rahul Anand <anand.rahul2886@gmail.com>
2023-02-05 16:56:09 +0000

    Initial commit

---
+++ src/main/java/com/anand/vriddhi/VriddhiApplication.java
@@ -0,0 +1,13 @@
+package com.anand.vriddhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VriddhiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VriddhiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anand/vriddhi/VriddhiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anand.vriddhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VriddhiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
